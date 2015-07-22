aaf1fb33bed931887e94fafc43860620e9704100
MichaelVessia <michaelvessia@mail.adelphi.edu>
2015-07-21 23:34:00 -0400

    fixed indentation

--- PE0001.java
+++ PE0001.java
@@ -1,18 +1,18 @@
 public class PE0001 {
 
-				public static void main(String[] args){
-								System.out.println(new PE0001().solve());
+  public static void main(String[] args){
+    System.out.println(new PE0001().solve());
 
-				}
+  }
 
-				public int solve() {
-								int sum = 0;
-								for(int i = 0; i < 1000; i++){
-												if (i % 3 == 0 || i % 5 == 0){
-																sum += i;
-												}
-								}
-								return sum;
-				}
+  public int solve() {
+    int sum = 0;
+    for(int i = 0; i < 1000; i++){
+      if (i % 3 == 0 || i % 5 == 0){
+        sum += i;
+      }
+    }
+    return sum;
+  }
 
 }
