61121f0b336c029c8f6bae902003e9880c6615f4
matt matero <matthewmatero@mail.adelphi.edu>
2015-11-21 02:29:11 -0500

    Removed extra 1000

--- elixir/elixir-MM.exs
+++ elixir/elixir-MM.exs
@@ -1,1 +1,1 @@
-IO.puts (Enum.reduce Enum.filter(1..1000, &(rem(&1,3) == 0 || rem(&1,5) == 0)), 0 , &(&1+&2)) - 1000
+IO.puts (Enum.reduce Enum.filter(1..999, &(rem(&1,3) == 0 || rem(&1,5) == 0)), 0 , &(&1+&2))
